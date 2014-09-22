e54e341348362072e5a188597969c31a3c088544
Dmitri Rubinstein <rubinstein@cs.uni-saarland.de>
2014-09-22 11:37:35 +0200

    Changed license to LGPL 3

--- Benchmarks/RabbitMQJava/src/main/java/dfki/sb/rabbitmqjava/model/MarketDataEntry.java
+++ Benchmarks/RabbitMQJava/src/main/java/dfki/sb/rabbitmqjava/model/MarketDataEntry.java
@@ -1,9 +1,21 @@
 package dfki.sb.rabbitmqjava.model;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* KIARA - Middleware for efficient and QoS/Security-aware invocation of services and exchange of messages
+ *
+ * Copyright (C) 2014 German Research Center for Artificial Intelligence (DFKI)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
  */
 
 import java.io.Serializable;

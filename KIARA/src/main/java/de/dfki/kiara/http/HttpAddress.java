52cdd2e03a951d7570d914c606f27dcd77de1919
Dmitri Rubinstein <rubinstein@cs.uni-saarland.de>
2014-11-06 13:33:27 +0100

    Added Glob class.

--- KIARA/src/main/java/de/dfki/kiara/http/HttpAddress.java
+++ KIARA/src/main/java/de/dfki/kiara/http/HttpAddress.java
@@ -20,6 +20,7 @@ package de.dfki.kiara.http;
 import de.dfki.kiara.InvalidAddressException;
 import de.dfki.kiara.Transport;
 import de.dfki.kiara.TransportAddress;
+import de.dfki.kiara.util.Glob;
 import java.net.InetAddress;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -84,114 +85,6 @@ public class HttpAddress implements TransportAddress {
         return path;
     }
 
-    /**
-     * From http://stackoverflow.com/questions/1247772
-     *
-     * Converts a standard POSIX Shell globbing pattern into a regular
-     * expression pattern. The result can be used with the standard
-     * {@link java.util.regex} API to recognize strings which match the glob
-     * pattern.
-     * <p/>
-     * See also, the POSIX Shell language:
-     * http://pubs.opengroup.org/onlinepubs/009695399/utilities/xcu_chap02.html#tag_02_13_01
-     *
-     * @param pattern A glob pattern.
-     * @return A regex pattern to recognize the given glob pattern.
-     */
-    private static String convertGlobToRegex(String pattern) {
-        StringBuilder sb = new StringBuilder(pattern.length());
-        int inGroup = 0;
-        int inClass = 0;
-        int firstIndexInClass = -1;
-        char[] arr = pattern.toCharArray();
-        for (int i = 0; i < arr.length; i++) {
-            char ch = arr[i];
-            switch (ch) {
-                case '\\':
-                    if (++i >= arr.length) {
-                        sb.append('\\');
-                    } else {
-                        char next = arr[i];
-                        switch (next) {
-                            case ',':
-                                // escape not needed
-                                break;
-                            case 'Q':
-                            case 'E':
-                                // extra escape needed
-                                sb.append('\\');
-                            default:
-                                sb.append('\\');
-                        }
-                        sb.append(next);
-                    }
-                    break;
-                case '*':
-                    if (inClass == 0) {
-                        sb.append(".*");
-                    } else {
-                        sb.append('*');
-                    }
-                    break;
-                case '?':
-                    if (inClass == 0) {
-                        sb.append('.');
-                    } else {
-                        sb.append('?');
-                    }
-                    break;
-                case '[':
-                    inClass++;
-                    firstIndexInClass = i + 1;
-                    sb.append('[');
-                    break;
-                case ']':
-                    inClass--;
-                    sb.append(']');
-                    break;
-                case '.':
-                case '(':
-                case ')':
-                case '+':
-                case '|':
-                case '^':
-                case '$':
-                case '@':
-                case '%':
-                    if (inClass == 0 || (firstIndexInClass == i && ch == '^')) {
-                        sb.append('\\');
-                    }
-                    sb.append(ch);
-                    break;
-                case '!':
-                    if (firstIndexInClass == i) {
-                        sb.append('^');
-                    } else {
-                        sb.append('!');
-                    }
-                    break;
-                case '{':
-                    inGroup++;
-                    sb.append('(');
-                    break;
-                case '}':
-                    inGroup--;
-                    sb.append(')');
-                    break;
-                case ',':
-                    if (inGroup > 0) {
-                        sb.append('|');
-                    } else {
-                        sb.append(',');
-                    }
-                    break;
-                default:
-                    sb.append(ch);
-            }
-        }
-        return sb.toString();
-    }
-
     @Override
     public boolean acceptsTransportConnection(TransportAddress transportAddress) {
         if (transportAddress == null) {
@@ -229,7 +122,7 @@ public class HttpAddress implements TransportAddress {
         final String otherPath = other.getPath();
         final String thisPath = getPath();
 
-        if (!otherPath.equals(thisPath) && !Pattern.matches(convertGlobToRegex(thisPath), otherPath)) {
+        if (!otherPath.equals(thisPath) && !Pattern.matches(Glob.convertGlobToRegex(thisPath), otherPath)) {
             return false;
         }
 
